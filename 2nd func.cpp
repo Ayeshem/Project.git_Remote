83ded795595dd6e8e8982efda5833efd6a3c4394
AyeshemIkram <ikramayehsa@gmail.com>
2023-09-17 17:20:26 +0500

    create second function

---
+++ 2nd func.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+void helo()
+{
+    cout<<"hello"<<endl;
+}
+
+int main()
+{
+    
+    helo();
+    
+    cout<<"Hello world!"<<endl;
+    cout<<"GitHub"<<endl;
+    
+    return 0;
+}
