fcd32a694d75773e62223ebae3a4bf432f6243e6
AyeshemIkram <ikramayehsa@gmail.com>
2023-09-17 17:08:05 +0500

    change text content

--- Project.git/main.cpp
+++ Project.git/main.cpp
@@ -1,14 +1,9 @@
-//
-//  main.cpp
-//  Project.git
-//
-//  Created by MBP on 17/09/2023.
-//
-
 #include <iostream>
+using namespace std;
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+int main()
+{
+    cout<<"Hello world!"<<endl;
+    
     return 0;
 }
