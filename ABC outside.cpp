6d5d41162ab6ff67753a9f5c7b2e36e380ef88c4
AyeshemIkram <ikramayehsa@gmail.com>
2023-09-19 13:07:25 +0500

    ABC OUTSIDE

---
+++ ABC outside.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+void helo()
+{
+    cout<<"hello"<<endl;
+}
+
+int main()
+{
+    cout<<"Changes made"<<endl;
+    cout<<"ABC ";
+    helo();
+    cout<<"Hello world!"<<endl;
+    cout<<"GitHub"<<endl;
+    
+    return 0;
+}
+
